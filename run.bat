2e6e1292d6117329d748b009398bc740d4754edf
DarthThanatos <robert.bielas@interia.pl>
2018-05-17 22:39:05 +0200

    init

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 md bin
 md lib
 dir /s /B *.java > sources
-call gradle build
-call gradle copyRuntimeLibs
+call gradlew build
+call gradlew copyRuntimeLibs
 javac -d bin -cp "lib/*;bin" @sources
 java -cp "bin;lib/*;src/main/resources" pl.agh.edu.ethereumreservations.EthereumApplication
 rem java -cp "out/production/classes;lib/*" pl.agh.edu.ethereumreservations.EthereumApplication
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 mkdir bin
 mkdir lib
 find -name "*.java" > sources
-gradle build
-gradle copyRuntimeLibs
+gradlew build
+gradlew copyRuntimeLibs
 javac -d bin -cp "lib/*:bin" @sources
 java -cp "bin:lib/*" pl.agh.edu.ethereumreservations.EthereumApplication
 # java -cp "out/production/classes:lib/*" pl.agh.edu.ethereumreservations.EthereumApplication
