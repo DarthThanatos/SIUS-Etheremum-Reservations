a36f7420eb6b27ad74a6ac593c4b0e66f250726c
burkat <burkat.krzysztof@gmail.com>
2018-05-24 18:59:12 +0200

    Sending json instead of String.

--- src/main/java/pl/agh/edu/ethereumreservations/rest/AccountController.java
+++ src/main/java/pl/agh/edu/ethereumreservations/rest/AccountController.java
@@ -1,9 +1,15 @@
 package pl.agh.edu.ethereumreservations.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.agh.edu.ethereumreservations.rest.pojo.Account;
 import pl.agh.edu.ethereumreservations.services.ether_service.IEthereumService;
 
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @RestController
@@ -22,10 +28,18 @@ public class AccountController {
         return ethereumService.getAccountsDescList();
     }
 
+    //e.g. localhost:8080/accounts/bob
     @GetMapping("/accounts/{userName}")
-    public String getAccount(@PathVariable("userName") String userName) {
-        //e.g. localhost:8080/accounts/bob
-        return ethereumService.getAccountDesc(userName);
+    @Produces({MediaType.APPLICATION_JSON})
+    public Account getAccount(@PathVariable("userName") String userName) {
+        Account account = new Account();
+        String[] accountInfo = ethereumService.getAccountDesc(userName).split(" ");
+        if (accountInfo.length == 6) {
+            account.setName(userName);
+            account.setPublicKey(accountInfo[2]);
+            account.setPrivateKey(accountInfo[5]);
+        }
+        return account;
     }
 
     @PutMapping("/accounts/new/{userName}")
---
+++ src/main/java/pl/agh/edu/ethereumreservations/rest/pojo/Account.java
@@ -0,0 +1,32 @@
+package pl.agh.edu.ethereumreservations.rest.pojo;
+
+public class Account {
+
+    private String name;
+    private String privateKey;
+    private String publicKey;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(String privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+}
